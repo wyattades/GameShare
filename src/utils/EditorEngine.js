3068cba9cc86203d1d0354e7fb735c090ac0ce22
Patrick Landis <pdlandis@ucsc.edu>
2017-10-20 12:46:30 -0700

    beginning move of object creation functions into engine class, merging instance class into engine class

--- src/utils/EditorEngine.js
+++ src/utils/EditorEngine.js
@@ -1,7 +1,7 @@
 import * as Pixi from 'pixi.js';
 import Colors from './Colors';
-// User variables for editor.
 
+// Stores and controls instance variables.
 class EditorInstance {
   constructor() {
     this.selectedObject = null;
@@ -58,7 +58,8 @@ function onDragMove() {
   }
 }
 
-export const createObject = ({ x = 0, y = 0, w = 1, h = 1, draggable, container, selectable }) => {
+export const createObject = ({ x = 0, y = 0, w = 1, h = 1, engine = null,
+  draggable, container, selectable }) => {
 
   const obj = new Pixi.Graphics();
 
@@ -105,18 +106,24 @@ export const createObject = ({ x = 0, y = 0, w = 1, h = 1, draggable, container,
     obj.interactive = true;
     obj.buttonMode = true;
 
-    obj
-    // events for drag start
-    .on('mousedown', onDragStart)
-    .on('touchstart', onDragStart)
-    // events for drag end
-    .on('mouseup', onDragEnd)
-    .on('mouseupoutside', onDragEnd)
-    .on('touchend', onDragEnd)
-    .on('touchendoutside', onDragEnd)
-    // events for drag move
-    .on('mousemove', onDragMove)
-    .on('touchmove', onDragMove);
+    if (engine) {
+      obj.on('mousedown', () => {
+        engine.objectClicked(obj);
+      });
+    } else {
+      obj
+      // events for drag start
+      .on('mousedown', onDragStart)
+      .on('touchstart', onDragStart)
+      // events for drag end
+      .on('mouseup', onDragEnd)
+      .on('mouseupoutside', onDragEnd)
+      .on('touchend', onDragEnd)
+      .on('touchendoutside', onDragEnd)
+      // events for drag move
+      .on('mousemove', onDragMove)
+      .on('touchmove', onDragMove);
+    }
   }
 
   return obj;
@@ -152,6 +159,15 @@ class Engine {
 
     this.container = options.container || createObject({ container: true });
     this.app.stage.addChild(this.container);
+/*
+    this.container.addObject(createRect({
+      x: 100, y: 100, w: 20, h: 20, draggable: true, fill: 0xFFAABB, stroke: 0x000000, engine: this,
+    }));
+*/
+    this.container.addObject(this.createRect_engine({ x: 50, y: 50, w: 20, h: 20, draggable: true, fill: 0xFFAABB, stroke: 0x000000 }));
+
+    this.selectedObject = null;
+    this.lockDragEvent = false;
   }
 
   addUpdate = fn => {
@@ -169,6 +185,138 @@ class Engine {
 
   translate = (x, y, z) => {
 
+  }
+
+  // TODO: rename
+  createObject_engine = ({ x = 0, y = 0, w = 1, h = 1, draggable, container,
+    selectable }) => {
+
+        const obj = new Pixi.Graphics();
+
+        if (selectable) {
+          obj.selectable = true;
+          obj.onSelectSet = () => {
+            obj.tint = Colors.GREEN;
+          };
+          obj.onSelectClear = () => {
+            obj.tint = Colors.WHITE;
+          };
+        }
+
+        if (container) {
+          obj.getChildren = () => obj.children;
+
+          obj.addObject = (...objs) => {
+            for (let o of objs) {
+              obj.addChild(o);
+            }
+          };
+
+          obj.removeObject = (...objs) => {
+            if (objs.length > 0) {
+              for (let o of objs) {
+                obj.removeChild(o);
+              }
+            } else {
+              obj.removeChildren();
+            }
+          };
+
+        }
+
+        obj.x = x;
+        obj.y = y;
+        obj.w = w;
+        obj.h = h;
+
+        if (draggable) {
+
+          obj.hitArea = new Pixi.Rectangle(0, 0, w, h);
+
+          obj.interactive = true;
+          obj.buttonMode = true;
+
+          let engine = this;
+
+          obj.on('mousedown', () => {
+            engine.objectMouseDown(obj);
+          });
+
+
+/*
+          if (engine) {
+            obj.on('mousedown', () => {
+              engine.objectClicked(obj);
+            });
+          }
+          else {
+            obj
+            // events for drag start
+            .on('mousedown', onDragStart)
+            .on('touchstart', onDragStart)
+            // events for drag end
+            .on('mouseup', onDragEnd)
+            .on('mouseupoutside', onDragEnd)
+            .on('touchend', onDragEnd)
+            .on('touchendoutside', onDragEnd)
+            // events for drag move
+            .on('mousemove', onDragMove)
+            .on('touchmove', onDragMove);
+          }
+          */
+        }
+
+
+
+        return obj;
+        //this.container.addObject(obj);
+      };
+  //}
+
+  createRect_engine = ({ w = 1, h = 1, fill, stroke, ...rest }) => {
+
+    const rect = this.createObject_engine({ w, h, ...rest });
+
+    if (typeof stroke === 'number') rect.lineStyle(1, stroke, 1);
+    if (typeof fill === 'number') rect.beginFill(fill);
+    rect.drawRect(0, 0, w, h);
+    rect.endFill();
+
+    return rect;
+  };
+
+  // Clear the current object selection.
+  clearSelection = () => {
+    if (this.selectedObject) {
+      if (this.selectedObject.onSelectClear) {
+        this.selectedObject.onSelectClear();
+      }
+    }
+    this.selectedObject = null;
+  }
+
+  // Clear the current selection, then select the given object.
+  selectObject = (obj) => {
+    this.clearSelection();
+    if (!obj.selectable) { return; }
+
+    this.selectedObject = obj;
+    if (this.selectedObject.onSelectSet) {
+      this.selectedObject.onSelectSet();
+    }
+  }
+
+  objectMouseDown = (obj) => {
+    console.log("engine.mousedown detected");
+    obj.tint = Colors.BLACK;
+  }
+  objectMouseUp = (obj) => {
+    console.log("engine.mouseup detected");
+    obj.tint = Colors.BLACK;
+  }
+  objectMouseMove = (obj) => {
+    console.log("engine.mousemove detected");
+    obj.tint = Colors.BLACK;
   }
 
 }
