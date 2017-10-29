05a11428e9b47fd354f1eb2dcbf4ddb2f0fd4f4d
Wyatt Ades <wyattades@gmail.com>
2017-10-29 01:59:20 -0700

    ejs --> pug, added new page templates

--- server/index.js
+++ server/index.js
@@ -31,18 +31,33 @@ app.use(express.static(path.resolve(__dirname, '../assets')));
 
 // TODO: generate file paths automatically
 const options = page => ({
+  page,
   title: `GameShare: ${page}`,
   css: DEV ? [] : ['/style.css'],
   rootContent: '',
-  script: DEV ? `/public/${page}.js` : `/${page}.js`,
+  scripts: [DEV ? `/public/${page}.js` : `/${page}.js`],
   rootId: 'root',
 });
 
-app.set('view engine', 'ejs');
+const staticOptions = page => ({
+  page,
+  title: page,
+  css: DEV ? [] : ['/style.css'],
+  scripts: DEV ? ['/public/loadStyles.js'] : undefined,
+});
+
+app.set('view engine', 'pug');
+app.locals.self = true; // Access variables in pug files from 'self' object e.g. self.title
 
 app.get('/', (req, res) => res.render('index', options('index')));
 app.get('/play', (req, res) => res.render('play', options('play')));
 app.get('/edit', (req, res) => res.render('edit', options('edit')));
+app.get('/games', (req, res) => res.render('games', staticOptions('games')));
+app.get('/login', (req, res) => res.render('login', staticOptions('login')));
+app.get('/register', (req, res) => res.render('register', staticOptions('register')));
+
+// 404 page
+app.use((req, res) => res.status(404).render('404', staticOptions('404')));
 
 
 // ---------- Listen on http server
