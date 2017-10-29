ba465f5f7dcfc320a15ff6cf9b45564ca2448cdb
shleewhite <leewhite128@gmail.com>
2017-10-29 15:04:31 -0700

    switched editor stuff to jquery

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,45 +1,6 @@
-import React, { Component } from 'react';
-
-import Engine, { createRect, createObject } from '../utils/EditorEngine';
+import $ from 'jquery';
+// import Engine, { createRect, createObject } from '../utils/EditorEngine';
 // import InputManager from '../utils/InputManager';
-
-
-const SNAP = 10;
-let GRID_X = 7000;
-let GRID_Y = 7000;
-
-// Create a draggable grid
-let grid = createObject({
-  x: 0, y: 0, w: GRID_X + 800, h: GRID_Y + 800, draggable: true, container: true
-});
-
-// Create a rectangle for the boundary of the game
-let boundary = grid.addObject(createRect({
-  x: 400, y: 400, w: GRID_X, h: GRID_Y, draggable: false, stroke: 0xFF0000, weight: 3
-}));
-
-function drawGrid() {
-  grid.w = GRID_X;
-  grid.h = GRID_Y;
-  console.log(grid.w, grid.h);
-
-  for (let x = 0; x < grid.w + 800; x += SNAP) {
-    if (x % 100 === 0) { grid.lineStyle(2, 0xAAAAAA, 1); }
-    else { grid.lineStyle(1, 0xAAAAAA, 1); }
-    grid.moveTo(x, 0);
-    grid.lineTo(x, grid.w + 800);
-  }
-  for (let y = 0; y < grid.h + 800; y += SNAP) {
-    if (y % 100 === 0) { grid.lineStyle(2, 0xAAAAAA, 1); }
-    else { grid.lineStyle(1, 0xAAAAAA, 1); }
-    grid.moveTo(0, y);
-    grid.lineTo(grid.h + 800, y);
-  }
-
-
-}
-
-drawGrid();
 
 // document.getElementById('obj-create').addEventListener('click', () => {
 //   grid.addObject(createRect({
@@ -57,34 +18,18 @@ drawGrid();
 //   drawGrid();
 // });
 
-export default class extends Component {
+$('#object-tab').click(function() {
+  $('#object-tab').addClass('is-active');
+  $('#grid-tab').removeClass('is-active');
+  $('.object-block').css('display', 'block');
+  $('.grid-block').css('display', 'none');
+});
 
-  componentDidMount() {
+$('#grid-tab').click(function() {
+  $('#object-tab').removeClass('is-active');
+  $('#grid-tab').addClass('is-active');
+  $('.object-block').css('display', 'none');
+  $('.grid-block').css('display', 'block');
+});
 
-    this.app = new Engine(this._parent, {
-      container: grid,
-    });
-    this.init();
-    this.app.start();
 
-    grid.position.x = -400;
-    grid.position.y = -400;
-  }
-
-  componentWillUnmount() {
-    this.app.stop();
-  }
-
-  init = () => {
-    // Add example object
-    grid.addObject(createRect({
-      x: 450, y: 450, w: 80, h: 100, draggable: true, fill: 0xFFAABB, stroke: 0x000000,
-    }));
-  }
-
-  render() {
-    return (
-      <div ref={_ => { this._parent = _; }}/>
-    );
-  }
-}
--- src/edit.js
+++ src/edit.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-
-// import logo from './assets/controller.png';
+import $ from 'jquery';
 import './styles/styles.scss';
-import App from './components/App';
 
-const _render = RootComponent => {
-  render((
-    <AppContainer>
-      <RootComponent/>
-    </AppContainer>
-  ), document.getElementById('root'));
-};
+import Engine, { createRect, createObject } from './utils/EditorEngine';
+import './components/Editor';
 
-_render(App);
+let app;
+const SNAP = 10;
+let GRID_X = 7000;
+let GRID_Y = 7000;
 
-// Enable hot reloading
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./components/App', () => {
-    // eslint-disable-next-line global-require
-    const NextRootContainer = require('./components/App').default;
-    _render(NextRootContainer);
-  });
+// Create a draggable grid
+let grid = createObject({
+  x: 0, y: 0, w: GRID_X + 800, h: GRID_Y + 800, draggable: true, container: true
+});
+
+// Create a rectangle for the boundary of the game
+let boundary = grid.addObject(createRect({
+  x: 400, y: 400, w: GRID_X, h: GRID_Y, draggable: false, stroke: 0xFF0000, weight: 3
+}));
+
+function drawGrid() {
+  grid.w = GRID_X;
+  grid.h = GRID_Y;
+  // console.log(grid.w, grid.h);
+
+  for (let x = 0; x < grid.w + 800; x += SNAP) {
+    if (x % 100 === 0) { grid.lineStyle(2, 0xAAAAAA, 1); }
+    else { grid.lineStyle(1, 0xAAAAAA, 1); }
+    grid.moveTo(x, 0);
+    grid.lineTo(x, grid.w + 800);
+  }
+  for (let y = 0; y < grid.h + 800; y += SNAP) {
+    if (y % 100 === 0) { grid.lineStyle(2, 0xAAAAAA, 1); }
+    else { grid.lineStyle(1, 0xAAAAAA, 1); }
+    grid.moveTo(0, y);
+    grid.lineTo(grid.h + 800, y);
+  }
 }
+
+drawGrid();
+
+app = new Engine($('#root').get(0), {
+  container: grid,
+});
+
+grid.addObject(createRect({
+  x: 450, y: 450, w: 80, h: 100, draggable: true, fill: 0xFFAABB, stroke: 0x000000,
+}));
+
+app.start();
+
+grid.position.x = -400;
+grid.position.y = -400;
