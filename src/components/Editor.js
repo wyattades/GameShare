700d0259534a2cca38caa962b3ad219d690c17a3
shleewhite <leewhite128@gmail.com>
2017-10-30 12:04:53 -0700

    code quality

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,14 +1,12 @@
 import $ from 'jquery';
 // import pug from 'pug';
 
-// const pug = require('pug');
 // const compiledType = pug.compileFile('type.pug');
 
 // import { createRect, createObject } from '../utils/EditorEngine';
-// import InputManager from '../utils/InputManager';
 
 // Handles displaying content for object/grid tabs
-$('#object-tab').click(function() {
+$('#object-tab').click(() => {
   $('#object-tab').addClass('is-active');
   $('#grid-tab').removeClass('is-active');
   $('#level-tab').removeClass('is-active');
@@ -17,7 +15,7 @@ $('#object-tab').click(function() {
   $('.level-block').css('display', 'none');
 });
 
-$('#grid-tab').click(function() {
+$('#grid-tab').click(() => {
   $('#object-tab').removeClass('is-active');
   $('#grid-tab').addClass('is-active');
   $('#level-tab').removeClass('is-active');
@@ -26,7 +24,7 @@ $('#grid-tab').click(function() {
   $('.level-block').css('display', 'none');
 });
 
-$('#level-tab').click(function() {
+$('#level-tab').click(() => {
   $('#object-tab').removeClass('is-active');
   $('#grid-tab').removeClass('is-active');
   $('#level-tab').addClass('is-active');
@@ -36,20 +34,20 @@ $('#level-tab').click(function() {
 });
 
 // Handles displaying object settings panel
-$('.object-button').click(function() {
+$('.object-button').click(() => {
   $('.object-settings').css('display', 'block');
 });
 
-$(document).click(function(event) {
+$(document).click((event) => {
   if ($(event.target).attr('class') !== 'object-button' && $('.object-settings').css('display') === 'block') {
     $('.object-settings').css('display', 'none');
   }
 });
 
 // Handles adding type to sidebar
-$('#new-type-button').click(function() {
+$('#new-type-button').click(() => {
   let typeName = prompt('Enter new object type name:');
-  // let typeHTML = compiledType({ name: typeName });
-  let typeHTML = '<div class="object-block panel-block"><span class="type-name">' + typeName + '</span><input type="color" name="type color" id="type-color" value="#FFAABB" style="margin-left: auto;"><div class="field has-addons"><div class="control"><button class="button is-small" name="rename type"><i class="fa fa-pencil"></i></button></div><div class="control"><button class="button is-small" name="delete type"><i class="fa fa-trash"></i></button></div></div></div>'
+  let typeHTML = compiledType({ name: typeName });
+  // let typeHTML = '<div class="object-block panel-block"><span class="type-name">' + typeName + '</span><input type="color" name="type color" id="type-color" value="#FFAABB" style="margin-left: auto;"><div class="field has-addons"><div class="control"><button class="button is-small" name="rename type"><i class="fa fa-pencil"></i></button></div><div class="control"><button class="button is-small" name="delete type"><i class="fa fa-trash"></i></button></div></div></div>'
   $(typeHTML).insertBefore('#new-buttons');
 });
--- src/edit.js
+++ src/edit.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 import './styles/styles.scss';
 
-import Engine, { generateGrid } from './utils/EditorEngine';
+import Engine from './utils/EditorEngine';
 import './components/Editor';
 
 let app = new Engine($('#root').get(0));
 app.start();
 
-$('#new-object-button').click(function() {
+$('#new-object-button').click(() => {
   // grid.addObject(createRect({
   //   x: 500, y: 500, w: 80, h: 80, draggable: true, fill: 0xFFAABB, stroke: 0x000000,
   // }));
