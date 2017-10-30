e73039679f764e75458e9a5e39fdd120f3cf9480
Wyatt Ades <wyattades@gmail.com>
2017-10-30 10:57:57 -0700

    add boilerplate for each page

--- src/loadStyles.js
+++ src/loadStyles.js
@@ -1,1 +1,8 @@
 import './styles/styles.scss';
+
+// // TEMP: Store values of loggedIn checkbox in localStorage
+// const $loggedIn = document.getElementById('loggedIn');
+// $loggedIn.checked = localStorage.getItem('loggedIn') === 'true';
+// $loggedIn.onclick = () => {
+//   localStorage.setItem('loggedIn', $loggedIn.checked ? 'true' : 'false');
+// };
