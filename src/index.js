6194f8f9514c035b535fbde3dbe6b4ac5f1d8315
Wyatt Ades <wyattades@gmail.com>
2017-11-01 14:12:12 -0700

    Added simple Games page, added temp logout button

--- src/index.js
+++ src/index.js
@@ -6,5 +6,14 @@ const googleSignIn = e => {
   // Sign in with Google:
 
 };
-
 document.getElementById('google-sign-in').addEventListener('click', googleSignIn);
+
+// TEMP
+const logout = e => {
+  e.target.removeEventListener(e.type, logout);
+
+  // Logout of firebase:
+
+};
+document.getElementById('google-sign-in').addEventListener('click', googleSignIn);
+
