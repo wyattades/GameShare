3731d3db82f3544a72aec064d76119baa5b6d5b0
Wyatt Ades <wyattades@gmail.com>
2017-11-04 19:33:18 -0700

    commented database functions

--- src/utils/db.js
+++ src/utils/db.js
@@ -13,8 +13,10 @@ firebase.initializeApp(config);
 const db = firebase.database(),
       auth = firebase.auth();
 
+// Presist data in the browser
 auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 
+// User authentication providers
 const providers = {
   google: new firebase.auth.GoogleAuthProvider(),
   github: new firebase.auth.GithubAuthProvider(),
@@ -40,36 +42,45 @@ export const assertLoggedIn = () => new Promise((resolve, reject) => {
 export const fetchUser = () => {
   const uid = auth.currentUser.uid;
 
-  return db.ref(`/users/${uid}`).once('value')
+  // Fetch user profile
+  return db.ref(`/users/${uid}/profile`).once('value')
   .then(snapshot => {
     const userData = snapshot.val();
-    if (!userData) {
+
+    if (!userData) { // If profile doesn't exist, create a new one    
       const newData = {
         username: auth.currentUser.displayName,
         email: auth.currentUser.email,
       };
-      return db.ref(`users/${uid}`).set(newData)
-      .then(() => Promise.resolve(newData));
-    } else {
-      return Promise.resolve(userData);
+
+      return db.ref(`users/${uid}/profile`).set(newData)
+      .then(() => newData); // return profile data
+
+    } else { // else, return profile data
+      return userData;
     }
   });
 };
 
+// Sign out of firebase
 export const logout = () => auth.signOut();
 
+// Login to firebaes
 export const login = provider => auth.signInWithPopup(providers[provider])
 .then(fetchUser);
 
+// Create a new game with the given data and status
 export const createGame = (data, status) => {
   const uid = auth.currentUser.uid;
 
   // Set a reference to the owner of the game
   data.uid = uid;
 
+  // Push new game data
   return db.ref('/games').push(data)
   .then(res => {
     const id = res.key;
+
     const created_on = Date.now();
     const info = {
       created_on,
@@ -77,31 +88,25 @@ export const createGame = (data, status) => {
       status: status || 'stopped',
     };
 
+    // Register user as owner of game
     db.ref(`/users/${uid}/games`).child(id).set(info);
 
     return { id, ...info };
   });
 };
 
-export const updateGame = (id, data, status) => {
-  if (data) {
-    return db.ref(`/games/${id}`).set(data)
-    .then(() => {
-      if (status) {
-        return db.ref(`/users/${auth.currentUser.uid}/games/${id}`).update({
-          status,
-          last_modified: Date.now(),
-        });
-      }
-      return Promise.resolve();
-    });
-  }
-  return Promise.resolve();
-};
+// Update game data/status
+export const updateGame = (id, data, status) => data ? db.ref(`/games/${id}`).set(data) : Promise.resolve()
+.then(() => status ? db.ref(`/users/${auth.currentUser.uid}/games/${id}`).update({
+  status,
+  last_modified: Date.now(),
+}) : Promise.resolve());
 
+// Delete game
 export const deleteGame = id => db.ref(`/games/${id}`).remove()
 .then(() => db.ref(`/users/${auth.currentUser.uid}/games/${id}`).remove());
 
+// Fetch user's games
 export const fetchGames = () => db
 .ref(`/users/${auth.currentUser.uid}/games`)
 .once('value')
@@ -110,4 +115,4 @@ export const fetchGames = () => db
 
   return Object.keys(games).map(id => ({ id, ...games[id] }));
 })
-.then(games => games.sort((a, b) => a.last_modified > b.last_modified));
+.then(games => games.sort((a, b) => a.last_modified > b.last_modified)); // Sort by last_modified time
