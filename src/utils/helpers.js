95dbecb8707bb9073d899d328d9d37459e8a46c3
Wyatt Ades <wyattades@gmail.com>
2017-11-27 10:23:28 -0800

    Renamed helpers files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,13 @@
 // Helper functions for converting: hex color string <--> color integer
-
 export const intToHex = int => {
   const hexString = `000000${((int) >>> 0).toString(16)}`.slice(-6);
   return `#${hexString}`;
 };
-
 export const hexToInt = hex => parseInt(hex.substring(1), 16);
+
+// Constrain val between min and max
+export const constrain = (val, min, max) => {
+  if (val < min) return min;
+  if (val > max) return max;
+  return val;
+};
