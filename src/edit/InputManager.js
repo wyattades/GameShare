3b75103715b798b984c00e830f2fa26e4ac27819
Wyatt Ades <wyattades@gmail.com>
2017-11-29 16:49:57 -0800

    TravisCI integration #6

--- src/edit/InputManager.js
+++ src/edit/InputManager.js
@@ -12,7 +12,7 @@ const keys = {
   39: [SPAN_SPEED, 0, 0], // right
   40: [0, SPAN_SPEED, 0], // down
   189: [0, 0, 1 - ZOOM_FACTOR], // minus
-  187: [0, 0, 1 + ZOOM_FACTOR], // plus  
+  187: [0, 0, 1 + ZOOM_FACTOR], // plus
 };
 
 const canvas = document.getElementById('root');
