5e4eabce84fe18b5ed408571ad061b97fc11123e
Wyatt Ades <wyattades@gmail.com>
2017-12-04 19:39:11 -0800

    fixed username test

--- __tests__/userName.test.js
+++ __tests__/userName.test.js
@@ -1,24 +1,24 @@
 const b = require('./browser');
 
+let firstDialog;
+
 describe('Create a Guest Username', () => {
 
   test('Go to specific game page', async () => { // Goto game in browser
     await b.page.goto(`${b.ROOT}/play/${b.TEST_GAME_ID}`);
-  }, 4000);
+    firstDialog = await b.once(b.page, 'dialog');
+  }, 8000);
 
   test('Enter Invalid Username', async () => {
-    const dialog = await b.once(b.page, 'dialog');
-    let popUp = await dialog.type(); // The dialog should be prompt
-    expect(popUp).toBe('prompt');
-    await dialog.accept('NameThatIsLongerThanTwentyCharacters'); // Upon entering an invalid name
-  }, 3000);
+    expect(firstDialog.type).toBe('prompt');
+    await firstDialog.accept('NameThatIsLongerThanTwentyCharacters'); // Upon entering an invalid name
+  }, 1000);
 
   let name = 'Valid Name';
 
   test('Enter Valid Username', async () => {
     const dialog = await b.once(b.page, 'dialog');
-    let popUp = await dialog.type(); // The dialog should be prompt
-    expect(popUp).toBe('prompt');
+    expect(dialog.type).toBe('prompt');
     await dialog.accept(name); // Upon entering a valid name
   }, 3000);
 
